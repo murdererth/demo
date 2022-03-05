27ffc5414793fc1fc0fbd69cc7467f795fe027d3
phanudet <phanudet.wong@gmail.com>
2022-03-05 17:33:27 +0700

    Creating first RESTful API
    1. ออกแบบ API
    
    GET /hello/{name}
    {
       "message" : "Hello John"
    }
    
    2. @RestController เพื่อบอกว่าเป็น Rest api
    3. install plugin RoboPOJOGenerator

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//    GET /hello/{name}
+//    {
+//        "message" : "Hello John"
+//    }
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse sayHi(@PathVariable String name) {
+        return new HelloResponse("Hello " + name);
+    }
+
+//    GET /api/users — เรียกดูรายการ User ทั้งหมด
+//    GET /api/users/{id} — เรียกดู User โดย user id
+//    GET /api/users?name=xxxx — เรียกดูรายการ User ด้วยการค้นหาด้วย name
+//    POST /api/users — สร้าง User ใหม่
+//    PUT /api/users/{id} — แก้ไข User โดย user id
+//    DELETE /api/users/{id} — ลบ User โดย user id
+
+    @GetMapping("/hello")
+    public HelloResponse sayHi2(@RequestParam(name = "id") String fooId, @RequestParam String name) {
+        return new HelloResponse("Hello " + name + ", id " + fooId);
+    }
+
+}
